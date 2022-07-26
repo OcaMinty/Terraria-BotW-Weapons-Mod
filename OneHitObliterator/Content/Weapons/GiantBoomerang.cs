c14f13cca0602601f5c3e9b38af4ab085b98ee63
OcaMinty <93810367+OcaMinty@users.noreply.github.com>
2022-07-25 22:38:01 -0400

    Update GiantBoomerang.cs

--- OneHitObliterator/Content/Weapons/GiantBoomerang.cs
+++ OneHitObliterator/Content/Weapons/GiantBoomerang.cs
@@ -6,8 +6,6 @@ using Microsoft.Xna.Framework;
 
 namespace OneHitObliterator.Content.Weapons
 {
-    // ExampleFlail and ExampleFlailProjectile show the minimum amount of code needed for a flail using the existing vanilla code and behavior. ExampleAdvancedFlail and ExampleAdvancedFlailProjectile need to be consulted if more advanced customization is desired, or if you want to learn more advanced modding techniques.
-    // ExampleFlail is a copy of the Sunfury flail weapon.
     internal class GiantBoomerang : ModItem
 	{
 		public override void SetStaticDefaults()
@@ -16,42 +14,37 @@ namespace OneHitObliterator.Content.Weapons
 			Tooltip.SetDefault("This is a modded flail with normal behavior.");
 			SacrificeTotal = 1;
 
-			// This line will make the damage shown in the tooltip twice the actual Item.damage. This multiplier is used to adjust for the dynamic damage capabilities of the projectile.
-			// When thrown directly at enemies, the flail projectile will deal double Item.damage, matching the tooltip, but deals normal damage in other modes.
 			ItemID.Sets.ToolTipDamageMultiplier[Type] = 2f;
 		}
 
 		public override void SetDefaults()
 		{
-			// These default values aside from Item.shoot match the Sunfury values, feel free to tweak them.
-			Item.useStyle = ItemUseStyleID.Shoot; // How you use the item (swinging, holding out, etc.)
-			Item.useAnimation = 45; // The item's use time in ticks (60 ticks == 1 second.)
-			Item.useTime = 45; // The item's use time in ticks (60 ticks == 1 second.)
-			Item.knockBack = 6.75f; // The knockback of your flail, this is dynamically adjusted in the projectile code.
-			Item.width = 30; // Hitbox width of the item.
-			Item.height = 10; // Hitbox height of the item.
-			Item.damage = 25; // The damage of your flail, this is dynamically adjusted in the projectile code.
+			Item.useStyle = ItemUseStyleID.Shoot;
+			Item.useAnimation = 45; 
+			Item.useTime = 45; 
+			Item.knockBack = 6.75f; 
+			Item.width = 30; 
+			Item.height = 10; 
+			Item.damage = 25; 
 			Item.knockBack = 40;
-			Item.crit = 7; // Critical damage chance %
+			Item.crit = 7;
 			Item.scale = 1.1f;
-			Item.noUseGraphic = true; // This makes sure the item does not get shown when the player swings his hand
-			Item.shoot = ModContent.ProjectileType<GiantBoomerangProjectile>(); // The flail projectile
-			Item.shootSpeed = 12f; // The speed of the projectile measured in pixels per frame.
-			Item.UseSound = SoundID.Item1; // The sound that this item makes when used
-			Item.rare = ItemRarityID.Orange; // The color of the name of your item
-			Item.value = Item.sellPrice(gold: 2, silver: 50); // Sells for 2 gold 50 silver
-			Item.DamageType = DamageClass.Melee; // Deals melee damage
+			Item.noUseGraphic = true;
+			Item.shoot = ModContent.ProjectileType<GiantBoomerangProjectile>();
+			Item.shootSpeed = 12f;
+			Item.UseSound = SoundID.Item1;
+			Item.rare = ItemRarityID.Orange;
+			Item.value = Item.sellPrice(gold: 2, silver: 50);
+			Item.DamageType = DamageClass.Melee;
 			Item.channel = true;
-			Item.noMelee = true; // This makes sure the item does not deal damage from the swinging animation
+			Item.noMelee = true;
 		}
 
 		public override Color? GetAlpha(Color lightColor)
 		{
-			// Aside from SetDefaults, when making a copy of a vanilla weapon you may have to hunt down other bits of code. This code makes the item draw in full brightness when dropped.
 			return Color.White;
 		}
 
-		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
 		public override void AddRecipes()
 		{
 			CreateRecipe()
