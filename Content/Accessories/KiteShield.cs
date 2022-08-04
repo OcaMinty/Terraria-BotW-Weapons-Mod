d237a6fb6634f6467ddd6d36f267b7d7c778ba03
OcaMinty <93810367+OcaMinty@users.noreply.github.com>
2022-08-04 12:16:23 -0400

    Update KiteShield.cs

--- Content/Accessories/KiteShield.cs
+++ Content/Accessories/KiteShield.cs
@@ -15,10 +15,6 @@ namespace OneHitObliterator.Content.Accessories
 
 			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
 
-			// These wings use the same values as the solar wings
-			// Fly time: 180 ticks = 3 seconds
-			// Fly speed: 9
-			// Acceleration multiplier: 2.5
 			ArmorIDs.Wing.Sets.Stats[Item.wingSlot] = new WingStats(180, 9f, 2.5f);
 		}
 
@@ -34,14 +30,13 @@ namespace OneHitObliterator.Content.Accessories
 		public override void VerticalWingSpeeds(Player player, ref float ascentWhenFalling, ref float ascentWhenRising,
 			ref float maxCanAscendMultiplier, ref float maxAscentMultiplier, ref float constantAscend)
 		{
-			ascentWhenFalling = 0.85f; // Falling glide speed
-			ascentWhenRising = 0.15f; // Rising speed
+			ascentWhenFalling = 0.85f;
+			ascentWhenRising = 0.15f;
 			maxCanAscendMultiplier = 1f;
 			maxAscentMultiplier = 3f;
 			constantAscend = 0.135f;
 		}
 
-		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
 		public override void AddRecipes()
 		{
 		}
